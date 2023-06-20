be58542671c029664a830515b6ffa4df343e5605
nhatminhdl <thinhatminhdl@gmail.com>
2023-06-20 22:45:31 +0700

    [Minh] 22-06-2023

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+//Http logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine("hbs", engine({extname: '.hbs'}));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'));
+
+//router
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.get("/news", (req, res) => {
+    res.render('news');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
