4e541be88afd0fbc988fcc70de5138c87c1ac007
nhatminhdl <thinhatminhdl@gmail.com>
2023-06-21 22:17:08 +0700

    [21-06-2023][Minh]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import { engine } from "express-handlebars";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 
@@ -11,21 +12,23 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 //Http logger
 app.use(morgan("combined"));
 
 //Template engine
-app.engine("hbs", engine({extname: '.hbs'}));
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resource/views'));
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resource/views"));
 
 //router
 app.get("/", (req, res) => {
-    res.render('home');
+    res.render("home");
 });
 
 app.get("/news", (req, res) => {
-    res.render('news');
+    res.render("news");
 });
 
 app.listen(port, () => {
