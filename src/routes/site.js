67c0db514100aac58df30aed0153631ea2a5260f
nhatminhdl <thinhatminhdl@gmail.com>
2023-06-25 19:51:57 +0700

    test husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,7 +3,7 @@ import SiteController from '../app/controllers/SiteController.js';
 
 const siteRouter = express.Router();
 
-                siteRouter.use('/search', SiteController.search);
-                siteRouter.use('/', SiteController.index);
+siteRouter.use('/search', SiteController.search);
+siteRouter.use('/', SiteController.index);
 
 export default siteRouter;
